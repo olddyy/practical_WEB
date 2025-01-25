50d25f808f4505ed5728f7cec938d4245750b64d
olddyy <158603693+olddyy@users.noreply.github.com>
2025-01-25 02:24:42 +0200

    add task 18

---
+++ 17_Objects_Object_constructor.js
@@ -0,0 +1,14 @@
+// function User (name, age){
+//     this.name = name;
+//     this.age = age;
+     
+//      this.sayHi = function(){
+//          alert("My name: " + this.name )
+//      }
+// }
+
+// let user  = new User ('Dmytro' , 19);
+// let user2 = new User ('Olya', 18);
+
+// user.sayHi();
+// console.log(user2);
---
+++ 18_Objects_Optional_Chain.js
@@ -0,0 +1,17 @@
+// const car1 = {
+//     country: 'Germany',
+//     brand:{
+//         model:'AMG' 
+//     }
+// } 
+
+// const car2 = {
+//     country: 'Italy',
+//     mark: 'Ferrary'
+// }
+
+// const getInfoCar = (obj) => {console.log(obj.brand?.model)}//?. -> проверка на сущевствование 
+
+
+// getInfoCar(car1)
+// getInfoCar(car2)
