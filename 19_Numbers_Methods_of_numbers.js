5d1cc21bc6e18cc460cb8eb4a9adb3be1ecf59fb
olddyy <158603693+olddyy@users.noreply.github.com>
2025-01-26 20:26:14 +0200

    add 19 lesson

---
+++ 19_Numbers_Methods_of_numbers.js
@@ -0,0 +1,63 @@
+// let billionFirst = 1000000000 // 1e9
+// let billionASecond = 1e9;
+
+// console.log(billionFirst)
+// console.log(billionSecond)
+
+
+/********************************* */
+// let num = 500
+
+// //String(num)
+// console.log(typeof num)
+// console.log(typeof num.toString())//toString
+
+//console.log(typeof 444..toString())
+
+/******************************************/
+
+// let num = 5.4
+
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+
+
+/******************************************/
+ 
+// let num = 4.586
+
+// console.log(num.toFixed(2))// string
+
+//
+/*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
+// console.log(0.1+0.2)//0.3....04  (binary bag)
+
+// let sum = 0.1+0.2
+// console.log(sum.toFixed(1))//0.3
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+// console.log(isNaN('str'))//true
+// console.log(NaN===NaN)//false
+// console.log(isFinite('fhfd'))//false
+// console.log(isFinite('32'))//true
+// console.log(Number.isNaN('asf'))//false
+
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!//
+
+// const price1 = '100$'
+
+// console.log(parseInt(price1))//only if number before character 
+
+// const price2 = '100.01$'
+
+// console.log(parseFloat(price2))//100.01
+
+//***********************************//
+
+// console.log((Math.random()*100).toFixed(0))
+
+
+// console.log(Math.max(2,34,14,32,0,-10))
+
+// console.log(Math.pow(3,3))
