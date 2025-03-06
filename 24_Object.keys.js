e631d674c68925bd79fd58f401c5b6dfda6fe150
olddyy <158603693+olddyy@users.noreply.github.com>
2025-03-06 19:32:58 +0200

    add 24 lesson

---
+++ 24_Object.keys.js
@@ -0,0 +1,34 @@
+// ////////////////////////keys,values,entries//////////////////////////////////
+
+// // const map = new Map([
+// //     ['key','value']
+// // ])
+
+// // map.keys()
+
+
+
+
+// const user = {
+//     name: "Vitaliy",
+//     age: 50,
+// }
+
+// // console.log(Object.keys(user))
+// //  console.log(Object.values(user))
+// //console.log(Object.entries(user))
+
+// const newArray = Object.entries(user).map((item) => {
+//     return ['key-prefix ' + item[0], 'value-prefix ' + item[1]]
+//     //console.log(item)
+//     //return
+// });
+// // console.log(newArray)
+
+
+
+
+
+// const newObj = Object.fromEntries(newArray);
+// console.log(user)
+// console.log(newObj)
