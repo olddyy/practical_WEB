6f908cd8b34bed36bed29fef79b6a6fe00627847
olddyy <158603693+olddyy@users.noreply.github.com>
2025-04-10 01:45:35 +0300

    add lessons

---
+++ 36_Error handling. The try - catch construct.js
@@ -0,0 +1,21 @@
+//Обработка ошибок
+
+
+// try {
+//     console.log(1);
+//     console.log(2);
+//     const user ={};
+//     console.log(user.name.er);
+    // if (!user.name){
+    //     throw new SyntaxError('Ошибка')
+    // }
+// } catch (error) {
+//     console.log("я допустил ошибку")
+//     console.log(error.name)
+//     console.log(error.message)
+    // console.log("error", error);
+// } finally {
+//     console.log("Я попал в finaly")
+// }
+
+// console.log(4)
---
+++ 37_Promise(1).js
@@ -0,0 +1,38 @@
+//Promise (Промисы)
+
+// let promise = new Promise((resolve, reject) => {
+//     //тело функции
+
+//     // resolve("111")
+
+//     setTimeout(() => {
+//         if (false) {
+//             resolve("Промис выполнился успешно");
+//         } else {
+//             reject(new Error("Упс"));
+
+//         }
+//         // reject(new Error("Упс"));
+//         // resolve("Промис выполнился успешно");
+//     }, 1000);
+// });
+
+// promise.then(
+//     (data) => console.log(data))
+//     .catch((error) => console.log(error))
+//     .finally(() => console.log("Finaly"));
+
+//пример
+
+// 
+
+
+
+
+// let promise = new Promise((resolve) => {
+// resolve();
+// });
+
+// promise
+//     .then((result) => dsadasd)
+//     .catch((error) => console.log(error));
